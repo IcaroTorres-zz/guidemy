93acf6333e18add12872280f3486c48f22b0ba4f
ICARO "STUART" DA SILVA TORRES <icaro.stuart@gmail.com>
2018-10-10 18:20:10 -0300

    card background

--- components/memberResult.vue
+++ components/memberResult.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <v-card :class="{'pa-4 mx-2 mt-5 text-xs-center': true, 'secondary darken-1':lightOut, 'grey lighten-3': !lightOut}" -->
-  <v-card class="pa-4 mx-2 mt-5 text-xs-center member-result" width="360px">
+  <v-card :class="{'pa-4 mx-2 mt-5 text-xs-center member-result': true, 'dark': lightOut, 'light': !lightOut}" width="360px">
     <v-layout column align-center justify-center style="margin-top: -64px">
         <nuxt-link :to="{name: 'user', params: {user: member.username}}">
           <v-avatar size="80px" :class="`elevation-${hover}`" @mouseover="hover = 15" @mouseout="hover = 0">
@@ -83,7 +83,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 export default {
   props: {
     member: {
@@ -103,6 +103,7 @@ export default {
     hover: 0
   }),
   computed: {
+    ...mapState(['lightOut']),
     ...mapGetters([
       'memberScoreForTasks',
       'memberScoreForDailies',
@@ -139,8 +140,19 @@ export default {
 </script>
 
 <style scoped>
-.member-result {
-  background: url(/assets/33.jpg) 40% 30% no-repeat;
+.member-result.dark {
+  background: url(http://coffecase.win/77/abc1f/material-design-wallpaper-red-034-by-charlie-henson_material-design-wallpaper-red-by-charlie-henson-devia-on-material-wallpapers-collection-for-free-downl.png) 59% 60% no-repeat;
+  background: url(https://www.vactualpapers.com/wallpapers/material-design-hd-wallpaper-no-0196/download/1920x1080.png) 59% 60% no-repeat;
   background-size: auto;
+}
+.member-result.light {
+  background: url(https://images.unsplash.com/photo-1520588831435-1529e6d7cf5e?ixlib=rb-0.3.5&s=7b9b8daa4ec154bb3d599ddeb16fffec&auto=format&fit=crop&w=984&q=80) 50% 50% no-repeat;
+  background: url(https://spayce.me/wp-content/uploads/2018/02/4k-wallpaper-android-material-design-wallpaper-full.jpg) 25% 15% no-repeat;
+  background: url(https://wallpaper-house.com/data/out/7/wallpaper2you_166304.png) 100% 0% no-repeat;
+  background: url(http://coffecase.win/77/48fda/material-design-android-6-0-marshmallow-material-design-android-6-0-marshmallow_x-material-design-android-marshmallow-on-material-wallpap.jpg) 45% 100% no-repeat;
+  background: url(https://besthqwallpapers.com/img/original/33662/4k-material-design-lines-geometry-triangles.jpg) 25% 0% no-repeat;
+  background: url(http://getwallpapers.com/wallpaper/full/a/3/3/432019.jpg) 25% 0% no-repeat;
+  /* background-size:300%; */
+  background-size:cover;
 }
 </style>
